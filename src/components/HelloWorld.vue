9d55834447d34d7af6429b14eb730ffadb12d9f7
Chris-Robin Ennen <chris@ncteam.de>
2021-06-06 00:51:36 +0200

    add tailwind example styles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
-  <h1>{{ msg }}</h1>
-
+  <h1 class="text-2xl font-medium font-serif">{{ msg }}</h1>
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
       Vite Documentation
@@ -8,22 +7,41 @@
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
-
-  <button @click="state.count++">count is: {{ state.count }}</button>
+  <button
+    class="
+      my-4
+      px-4
+      py-2
+      rounded
+      ring-black ring-1
+      bg-white
+      hover:bg-gray-100
+      transition-colors
+    "
+    @click="state.count++"
+  >
+    count is: {{ state.count }}
+  </button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
+<script>
+import { reactive } from "vue";
 
-defineProps({
-  msg: String
-})
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  setup(props) {
+    const state = reactive({ count: 0 });
 
-const state = reactive({ count: 0 })
+    return { state };
+  },
+};
 </script>
 
 <style scoped>
