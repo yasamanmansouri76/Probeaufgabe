1c68b3b58798ebb7a83f81390cf86d872266a286
Chris-Robin Ennen <chris@ncteam.de>
2021-10-25 12:15:09 +0200

    add jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: [
+    './*.html',
+    './src/**/*.{js,ts,vue}'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
